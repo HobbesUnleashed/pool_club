f6de1223336c5835fa97f28b04606ccfa96d58dd
Shay <sleacy@googlemail.com>
2025-04-09 09:20:21 +0100

    Prepare for Database creation

--- pool_club/settings.py
+++ pool_club/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
+
+if os.path.isfile("env.py"):
+    import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +25,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-l9p3u8rg1lr$zma5r)49$j1o#xfj0t0#ww40)o&)*w%#_218^p"
+# SECRET_KEY = "django-insecure-l9p3u8rg1lr$zma5r)49$j1o#xfj0t0#ww40)o&)*w%#_218^p"
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -74,12 +80,14 @@ WSGI_APPLICATION = "pool_club.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
+"""DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
     }
-}
+}"""
+
+DATABASES = {"default": dj_database_url.parse(os.environ.get("DATABASE_URL"))}
 
 
 # Password validation
